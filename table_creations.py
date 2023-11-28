c3dc1ab05ff0860cc866354788e6adb742699447
ivholmlu <ivholmlu@nmbu.no>
2023-11-28 11:24:23 +0100

    Adding initiation of cassandra driver sesssion

--- table_creations.py
+++ table_creations.py
@@ -1,11 +1,12 @@
-
+from cassandra.cluster import Cluster
 
 def reset_table(session, keyspace, table_name):
-    reset_query = f"DROP TABLE {keyspace}.{table_name};"
+    reset_query = f"DROP TABLE IF EXISTS {keyspace}.{table_name};"
     session.execute(reset_query)
 
 def create_locality_table(session):
 
+    session.set_keyspace('fish_data')
     table_creation_query = """
         CREATE TABLE IF NOT EXISTS locality_data (
             year INT,
@@ -32,10 +33,10 @@ def create_locality_table(session):
         );
     """
     session.execute(table_creation_query)
-    
-def create_locality_table(session):
-    session.execute("DROP TABLE id_35297")
 
+def create_localities_table(session):
+
+    session.execute("DROP TABLE locality_{locality_id}")
     table_creation_query = """
         CREATE TABLE locality_{locality_id} (
             datetime TEXT PRIMARY KEY,
@@ -46,5 +47,30 @@ def create_locality_table(session):
             seaTemperature FLOAT,
         )
         """
-
     session.execute(table_creation_query)
+
+def check_table_exist(session, keyspace):
+    session.set_keyspace('{keyspace}')
+    tables = session.execute("SELECT table_name FROM system_schema.tables WHERE keyspace_name = %s", [keyspace])
+    table_names = [row.table_name for row in tables]
+    table_exists = False
+    if table_to_check in table_names:
+        table_exists = True
+    return table_exists
+
+def create_keyspace(session, keyspace):
+    session.execute(
+        f"CREATE KEYSPACE IF NOT EXISTS {keyspace} "
+        "WITH REPLICATION = {"
+        "'class' : 'SimpleStrategy', "
+        "'replication_factor' : 1"
+        "};"
+    )
+
+def initiate_cassandra_driver(p=9042):
+    cluster = Cluster(['localhost'], port=p)
+    session = cluster.connect() 
+    return session
+
+def initiate_spark():
+    pass
