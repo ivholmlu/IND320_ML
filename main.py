6926ad510c5dcccd1d9313baff338df95ffce310
ivholmlu <ivholmlu@nmbu.no>
2023-11-30 18:57:45 +0100

    Adding slider

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import os
 from pyspark.sql import SparkSession
 from api_creations import *
 
-
-"""
 def _update_plotly_pd(state):
     df = state["click_df"]
     name = df['name'].values[0]
@@ -16,8 +14,15 @@ def _update_plotly_pd(state):
     pie_data.columns = ['haspd', 'count']
     fig_pd = px.pie(pie_data, names="haspd", values='count', title=f'Pie chart of hasPd for {name}')
     state["plotly_pd"] = fig_pd
-    print(fig_pd) """
-
+    state["test"] = "pd"
+    
+def _update_plotly_histogram(state):
+    df = state["click_df"]
+    print(df["avgadultfemalelice"].values)
+    name = df['name'].values[0]
+    fig_hist = px.line(df, x="week", y="avgadultfemalelice", title=f'histogram of avgadultfemalelice for {name}')
+    state["plotly_hist"] = fig_hist
+    state["test"] = "hist"
 
 def _update_plotly_localities(state):
     localities = state["localities_df"]
@@ -30,7 +35,7 @@ def _update_plotly_localities(state):
         lat="lat",
         lon="lon",
         hover_name="name",
-        hover_data=["lat","lon"],
+        hover_data=["lat","lon","localityno"],
         color_discrete_sequence=["darkgreen"],
         zoom=5,
         height=600,
@@ -42,24 +47,44 @@ def _update_plotly_localities(state):
     fig_localities.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     state["plotly_localities"] = fig_localities
 
-""" def handle_click(state, payload):
+def handle_click(state, payload):
     localities = state["localities_df"]
-    num = payload[0]["pointNumber"]
+    full_df = state["full_df"]
+    num = payload[0]["pointNumber"] #index of clicked point
     locality_no = localities.loc[num, 'localityno']
-    df = localities[localities['localityno'] == locality_no]
+    state["localityno"] = locality_no
+    df = full_df[full_df['localityno'] == locality_no]
     state["click_df"] = df
-    state["histo_columns"] = df.select_dtypes(include=['int', 'float']).columns
-    _update_plotly_pd(state) """
+    state["click_dfshape"] = f"{df.shape}"
+
+    _update_plotly_pd(state)
+    _update_plotly_histogram(state)
+
+
+    
+def _update_year(state):
+    df = localities_api(state["current_year"])
+    state["full_df"] = df 
+    state["full_dfshape"] = f"{df.shape}"
+    unique_df =  df.drop_duplicates(subset=['localityno', 'lat', 'lon', 'year', 'name'], inplace=False) #Values needed in map plot
+    state["localities_df"] = unique_df
+    state["localities_dfshape"] = f"{unique_df.shape}"
+    _update_plotly_localities(state)
+
+def handle_select(state, payload):
+    state["current_year"] = payload["value"]
+__  update_year(state)
 
 def _get_init_dataframe(state):
     create_locality_table()
     df = localities_api(2022)
-    state["localities_df"] = df
+    state["full_df"] = df 
+    state["full_dfshape"] = f"{df.shape}"
+    unique_df =  df.drop_duplicates(subset=['localityno', 'lat', 'lon', 'year', 'name'], inplace=False) #Values needed in map plot
+    state["localities_df"] = unique_df
+    state["localities_dfshape"] = f"{unique_df.shape}"
     insert_localities_year(state["localities_df"])
 
-
-# "_my_private_element" won't be serialised or sent to the frontend,
-# because it starts with an underscore (not used here)
 
 initial_state = ss.init_state({
     "my_app": {
