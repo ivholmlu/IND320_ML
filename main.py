6d857aebbad5d63e3fb1ba28dde13f3f0337fbc1
ivholmlu <ivholmlu@nmbu.no>
2023-12-02 12:58:17 +0100

    Working main with slider

--- main.py
+++ main.py
@@ -3,21 +3,84 @@ import pandas as pd
 import plotly.express as px
 from use_spark import *
 from table_creations import *
-from pyspark.sql import SparkSession
 from api_creations import *
 
+def _update_plotly_pd(state):
+    df = state["click_df"]
+    name = df['name'].values[0]
+    print(df["haspd"])
+    fig_pd = px.pie(df, names="haspd", title=f'Pie chart of hasPd for {name}')
+    state["plotly_pd"] = fig_pd
+    
+def _update_plotly_columns(state):
+    df = state["click_df"]
+    name = df['name'].values[0]
+    col = state["selected:column"]
+    fig_columns = px.line(df, x="week", y="avgadultfemalelice", title=f'Line chart of {col} for {name}')
+    state["plotly_line"] = fig_columns
 
-def _get_init_dataframe(state):
-    print("Starting _get_init_dataframe")
-    if not state["driver"].check_keyspace_exist("fish_data"):
-        state["driver"].create_keyspace("fish_data")
-    if not state["driver"].check_table_exist("locality_data"):
-        state["driver"].create_locality_table()
-    if state["driver"]._check_year("locality_data", 2020):
-        state["localities_df"] = state["driver"].get_localities_year(2022)
-    _get_localities(state, 2022) # TODO Add IF test to check if table exists
-    insert_localities_year(state["localities_df"])
-    print("Done with _get_init_dataframe") 
+def _update_plotly_localities(state):
+    localities = state["localities_df"]
+    selected_num = state["selected_num"]
+    sizes = [10]*len(localities)
+    if selected_num != -1:
+        sizes[selected_num] = 20
+    fig_localities = px.scatter_mapbox(
+        localities,
+        lat="lat",
+        lon="lon",
+        hover_name="name",
+        hover_data=["lat","lon"],
+        color_discrete_sequence=["darkgreen"],
+        zoom=5,
+        height=600,
+        width=700,
+    )
+    overlay = fig_localities['data'][0]
+    overlay['marker']['size'] = sizes
+    fig_localities.update_layout(mapbox_style="open-street-map")
+    fig_localities.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    state["plotly_localities"] = fig_localities
+
+def handle_click(state, payload):
+    localities = state["localities_df"]
+    full = state["full_df"]
+    num = payload[0]["pointNumber"]
+    locality_no = localities.loc[num, 'localityno']
+    df = full[full['localityno'] == locality_no]
+    state["click_df"] = df
+    _update_plotly_pd(state) 
+    _update_plotly_columns(state)
+
+def handle_select(state, payload):
+    print(payload)
+
+def _get_init_dataframes(state):
+    if not check_table_exist:
+        create_locality_table()
+    if not check_year("fish_data", "locality_data", 2022):
+        df = localities_api(2022)
+        insert_localities_year(df)
+        print("Initial dataframe created")
+    else:
+        df = get_localities(2022) #Via cassandra
+        print("Initial dataframe gathered")
+    unique_df = df.drop_duplicates(
+        subset=['localityno', 'lat', 'lon', 'year', 'name'],
+        inplace=False)
+    state["full_df"] = df
+    state["localities_df"] = unique_df
+
+def _get_JSON(state):
+    localitites = state["localities_df"]
+    # Create JSON with keys list(range(9)), and restaurant names as values
+    my_json = dict(zip(list(range(len(localitites))), localitites["name"].values))
+    # Convert keys to strings
+    my_json = {str(key): value for key, value in my_json.items()}
+    state["localitites_JSON"] = my_json
+    
+# "_my_private_element" won't be serialised or sent to the frontend,
+# because it starts with an underscore (not used here)
 
 initial_state = ss.init_state({
     "my_app": {
@@ -27,8 +90,8 @@ initial_state = ss.init_state({
     "selected":"Click to select",
     "selected_num":-1,
     "current_year" : 2022,
-    "driver" : Cassandra_Spark_Driver_Singleton()
 })
 
-_get_init_dataframe(initial_state)
-
+_get_init_dataframes(initial_state)
+_update_plotly_localities(initial_state)
+_get_JSON(initial_state)
--- table_creations.py
+++ table_creations.py
@@ -1,212 +1,114 @@
 from cassandra.cluster import Cluster
 import os
-from pyspark.sql import SparkSession
-import warnings
-from credentials import config, config_frost
-from authentication import get_token
 import pandas as pd
-import requests
-class Cassandra_Spark_Driver_Singleton:
-    _instance = None
 
-    def __init__(self):
-        self.keyspace = "fish_data"
-        self.token = get_token()
-        self.config = config
-        self.config_frost = config_frost
+def _initiate_cassandra_driver(p=9042):
+    print("CASSANDRA DRIVER INITIATED")
+    cluster = Cluster(['localhost'], port=p)
+    session = cluster.connect()
+    return session
 
-    def _reset_table(self, keyspace, table_name):
-        reset_query = f"DROP TABLE IF EXISTS {keyspace}.{table_name};"
-        self._session.execute(reset_query)
+def reset_table(keyspace, table_name):
+    session = _initiate_cassandra_driver()
+    reset_query = f"DROP TABLE IF EXISTS {keyspace}.{table_name};"
+    session.execute(reset_query)
 
-    def _initiate_cassandra_driver(self, port=9042):
-            print("Cassandra initiating...")
-            cluster = Cluster(['localhost'], port=port)
-            session = cluster.connect()
-            session.set_keyspace('fish_data')
-            print("Cassandra initiated")
-            return session
-    def _initiate_spark(self, port='9042'):
-        print("Spark initiating...")
-        spark = SparkSession.builder.appName('SparkCassandraApp').\
-            config('spark.jars.packages', 'com.datastax.spark:spark-cassandra-connector_2.12:3.4.1').\
-            config('spark.cassandra.connection.host', 'localhost').\
-            config('spark.sql.extensions', 'com.datastax.spark.connector.CassandraSparkExtensions').\
-            config('spark.sql.catalog.mycatalog', 'com.datastax.spark.connector.datasource.CassandraCatalog').\
-            config("spark.cassandra.output.batch.size.rows", "1000").\
-            config('spark.cassandra.connection.port', port).getOrCreate()
-        print("Spark initiated")
-        return spark
+def create_locality_table(session=1):
+    session = _initiate_cassandra_driver()
+    session.set_keyspace('fish_data')
+    session.execute(f"DROP TABLE IF EXISTS locality_data")
+    table_creation_query = """CREATE TABLE IF NOT EXISTS locality_data (
+    year INT,
+    week INT,
+    localityno INT,
+    localityweekid INT,
+    name TEXT,
+    hasreportedlice BOOLEAN,
+    isfallow BOOLEAN,
+    avgadultfemalelice DOUBLE,
+    hascleanerfishdeployed BOOLEAN,
+    hasmechanicalremoval BOOLEAN,
+    hassubstancetreatments BOOLEAN,
+    haspd BOOLEAN,
+    hasila BOOLEAN,
+    municipalityno TEXT,
+    municipality TEXT,
+    lat DOUBLE,
+    lon DOUBLE,
+    isonland BOOLEAN,
+    infilteredselection BOOLEAN,
+    hassalmonoids BOOLEAN,
+    isslaughterholdingcage BOOLEAN,
+    PRIMARY KEY (year, week, localityno)
+);"""
 
-    def create_locality_table(self,reset=False):
-        session = self._initiate_cassandra_driver()
-        if reset:
-            session.execute(f"DROP TABLE IF EXISTS locality_data")
-        session.set_keyspace('fish_data')
-        table_creation_query = """CREATE TABLE IF NOT EXISTS locality_data (
-        year INT,
-        week INT,
-        localityno INT,
-        localityweekid INT,
-        name TEXT,
-        hasreportedlice BOOLEAN,
-        isfallow BOOLEAN,
-        avgadultfemalelice DOUBLE,
-        hascleanerfishdeployed BOOLEAN,
-        hasmechanicalremoval BOOLEAN,
-        hassubstancetreatments BOOLEAN,
-        haspd BOOLEAN,
-        hasila BOOLEAN,
-        municipalityno TEXT,
-        municipality TEXT,
-        lat DOUBLE,
-        lon DOUBLE,
-        isonland BOOLEAN,
-        infilteredselection BOOLEAN,
-        hassalmonoids BOOLEAN,
-        isslaughterholdingcage BOOLEAN,
-        PRIMARY KEY (year, week, localityno)
-    );"""
+    session.execute(table_creation_query)
 
-        self.session.execute(table_creation_query)
+def create_localities_table(locality_id, keyspace="fish_data"):
+    session = _initiate_cassandra_driver()
+    session.set_keyspace(keyspace)
+    session.execute(f"DROP TABLE IF EXISTS locality_{locality_id}")
+    table_creation_query = f"""CREATE TABLE locality_{locality_id} (
+    year INT,
+    week INT,
+    localityno INT,
+    avgadultfemalelice FLOAT,
+    hasreportedlice BOOLEAN,
+    avgmobilelice FLOAT,
+    avgstationarylice FLOAT,
+    seatemperature FLOAT,
+    localityname TEXT,
+    lat FLOAT,
+    lon FLOAT,
+    PRIMARY KEY (year, week, localityno)
+);"""
+    session.execute(table_creation_query)
 
-    def _create_localities_table(self, reset=False):
-        session = self._initiate_cassandra_driver()
-        if reset:
-            session.execute(f"DROP TABLE IF EXISTS locality_id")
-        table_creation_query = f"""CREATE TABLE locality_id (
-        year INT,
-        week INT,
-        localityno INT,
-        avgadultfemalelice FLOAT,
-        hasreportedlice BOOLEAN,
-        avgmobilelice FLOAT,
-        avgstationarylice FLOAT,
-        seatemperature FLOAT,
-        localityname TEXT,
-        lat FLOAT,
-        lon FLOAT,
-        PRIMARY KEY (year, week, localityno)
-    );"""
-        session.execute(table_creation_query)
+def check_table_exist(keyspace, table_to_check):
+    session = _initiate_cassandra_driver()
+    session.set_keyspace('{keyspace}')
+    tables = session.execute("SELECT table_name FROM system_schema.tables WHERE keyspace_name = %s", [keyspace])
+    table_names = [row.table_name for row in tables]
+    table_exists = False
+    if table_to_check in table_names:
+        table_exists = True
+    return table_exists
 
+def create_keyspace(keyspace):
+    session = _initiate_cassandra_driver()
+    session.execute(
+        f"CREATE KEYSPACE IF NOT EXISTS {keyspace} "
+        "WITH REPLICATION = {"
+        "'class' : 'SimpleStrategy', "
+        "'replication_factor' : 1"
+        "};"
+    )
+
+def check_year(keyspace, table_name, year):
+    session = _initiate_cassandra_driver()
+    query = f"SELECT * FROM {keyspace}.{table_name} WHERE year = {year} ALLOW FILTERING"
+    rows = session.execute(query)
+    if rows:
+        return True
+    else:
+        return False
     
-    def check_table_exist(self, table_to_check):
-        session = self._initiate_cassandra_driver()
-        print(f"Checking if table {table_to_check} exists")
-        tables = session.execute(
-            "SELECT table_name FROM system_schema.tables\
-            WHERE keyspace_name = %s", [self.keyspace])
-        table_names = [row.table_name for row in tables]
-        table_exists = False
-        if table_to_check in table_names:
-            print(f"Table {table_to_check} exists")
-            table_exists = True
-        else:
-            print(f"Table {table_to_check} does not exist")
-        return table_exists
+def get_localities(year):
+    session = _initiate_cassandra_driver()
+    session.set_keyspace('fish_data')
+    query = f"SELECT * FROM locality_data WHERE year = {year}"
+    rows = session.execute(query)
+    #Return rows as df
+    df = pd.DataFrame(list(rows))
+    return df
     
-    def check_keyspace_exist(self, keyspace_to_check):
-        print(f"Checking if keyspace {keyspace_to_check} exists")
-        session = self._initiate_cassandra_driver()
-        keyspaces = session.execute("SELECT keyspace_name FROM system_schema.keyspaces")
-        keyspace_names = [row.keyspace_name for row in keyspaces]
-        keyspace_exists = False
-        if keyspace_to_check in keyspace_names:
-            print(f"Keyspace {keyspace_to_check} exists")
-            keyspace_exists = True
-        else:
-            print(f"Keyspace {keyspace_to_check} does not exist")
-        return keyspace_exists
-
-    def _create_keyspace(self, keyspace):
-        session = self._initiate_cassandra_driver()
-        session.execute(
-            f"CREATE KEYSPACE IF NOT EXISTS {keyspace} "
-            "WITH REPLICATION = {"
-            "'class' : 'SimpleStrategy', "
-            "'replication_factor' : 1"
-            "};"
-        )
-
-    def check_year(self, table_name, year):
-        session = self._initiate_cassandra_driver()
-        print(f"Checking if year {year} exists in table {table_name}")
-        query = f"SELECT * FROM {self.keyspace}.{table_name} WHERE year = {year} LIMIT 1;"
-        rows = session.execute(query)
-        if rows:
-            print(f"Year {year} exists in table {table_name}")
-            return True
-        else:
-            print(f"Year {year} does not exist in table {table_name}")
-            return False
-        
+def get_lat_lon(locality_id):
+    session = _initiate_cassandra_driver()
+    session.set_keyspace('fish_data')
+    query = f"SELECT lat, lon FROM locality_data WHERE year = 2022 AND week = 1 AND localityno = {locality_id}"
+    result = session.execute(query)
     
-    def _get_lat_lon(session, locality_id):
-        session.set_keyspace('fish_data')
-        query = f"SELECT lat, lon FROM locality_data WHERE year = 2022 AND week = 1 AND localityno = {locality_id}"
-        result = session.execute(query)
-        
-        for row in result:
-            lat = row.lat
-            lon = row.lon
-        return lat, lon
-    
-    def _get_week_summary(self, year, week):
-
-        url = f"{self.config['api_base_url']}/v1/geodata/fishhealth/locality/{year}/{week}"
-        headers ={
-            'authorization': 'Bearer ' + self.token['access_token'],
-            'content-type': 'application/json',
-        }
-
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return response.json()
-
-    def localities_api(self, year):
-        print(f"Getting localities for {year}") # TODO Add IF test to check if table exists
-        list_localitites = []
-        for week in range(1, 53):
-            locality_data = self._get_week_summary(year, week)
-            for row in locality_data["localities"]:
-                row["year"] = locality_data["year"]
-                row["week"] = locality_data["week"]
-                list_localitites.append(row)
-        df_localities_year = pd.DataFrame(list_localitites)
-        df_localities_year.columns = df_localities_year.columns.str.lower()
-        print(f"Fetched localities for {year}, returning dataframe")
-        return df_localities_year
-    
-    def get_localities(self, year):
-        print(f"_get_localities for {year}")
-        df = self.localities_api(year) 
-        return df
-    
-    def insert_localities_year(self, df):
-        spark = self._initiate_spark()
-    #insert/append into existing table for a yearly instance of data from all localitites.
-        print("Inserting into localities table")
-        spark_df = spark.createDataFrame(df)
-        spark_df.write\
-            .format("org.apache.spark.sql.cassandra")\
-            .mode('append')\
-            .options(table=f"locality_data", keyspace=self.keyspace)\
-            .save()
-        print("Insertion into localities done")
-        spark.stop()
-
-
-"""     def _initiate_spark_driver(self, port='9042'):
-        print("Spark initiating...")
-        warnings.simplefilter(action='ignore', category=FutureWarning)
-        os.environ["PYSPARK_PYTHON"] = r"/home/ivholmlu/miniconda3/envs/ind320/bin/python" 
-        os.environ["PYSPARK_DRIVER_PYTHON"] = r"/home/ivholmlu/miniconda3/envs/ind320/bin/python"   
-        self._spark = SparkSession.builder.appName('SparkCassandraApp').\
-            config('spark.jars.packages', 'com.datastax.spark:spark-cassandra-connector_2.12:3.4.1').\
-            config('spark.cassandra.connection.host', 'localhost').\
-            config('spark.sql.extensions', 'com.datastax.spark.connector.CassandraSparkExtensions').\
-            config('spark.sql.catalog.mycatalog', 'com.datastax.spark.connector.datasource.CassandraCatalog').\
-            config("spark.cassandra.output.batch.size.rows", "1000").\
-            config('spark.cassandra.connection.port', port).getOrCreate()
-        print("Spark initiated") """
+    for row in result:
+        lat = row.lat
+        lon = row.lon
+    return lat, lon
